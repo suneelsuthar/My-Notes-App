98170af7591cb5a6e8c2daa5c1b0ad2ebd8c468a
suneel kumar <suneelsuther123@gmail.com>
2022-10-23 07:22:06 +0500

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -4,13 +4,7 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDrZq_y0pU8vetAOW1ahCbg75NuKICCM6c",
-  authDomain: "restaurant-fb1bc.firebaseapp.com",
-  databaseURL: "https://restaurant-fb1bc-default-rtdb.firebaseio.com",
-  projectId: "restaurant-fb1bc",
-  storageBucket: "restaurant-fb1bc.appspot.com",
-  messagingSenderId: "160807831200",
-  appId: "1:160807831200:web:02efeae18f8f8e3456abc5",
+ 
 };
 
 // initialize firebase
